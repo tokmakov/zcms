f3a9aad82f672bcf4947bdf4be7b582de38dcf48
Evgeniy Tokmakov <tokmakov.e@mail.ru>
2015-12-25 16:25:22 +0300

    Незначительные изменения

--- view/tinko/frontend/resource/js/page/library.js
+++ view/tinko/frontend/resource/js/page/library.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
-    $('#library > div:first-of-type > div').hide();
-    $('#library > div:first-of-type > h3 > span').click(function() {
-        $(this).parent().next().slideToggle();
-    });
-    $('#library > div').hide();
-    $('#library > h2 > span').click(function() {
+    $('#library > div, #library > div:first-of-type > div').hide();
+    $('#library > h2 > span, #library > div:first-of-type > h3 > span').click(function() {
         $(this).parent().next().slideToggle();
     });
 });
