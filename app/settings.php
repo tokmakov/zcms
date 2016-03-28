9219724f255e46dd1c9d235993c7bc741255c018
Evgeniy Tokmakov <tokmakov.e@mail.ru>
2016-03-28 08:48:46 +0300

    Незначительные изменения

--- app/settings.php
+++ app/settings.php
@@ -82,16 +82,16 @@ $settings = array(
     ),
     'cdn' => array(                              // Content Delivery Network
         'enable' => array(
-            'js'     => true,                   // js-файлы
-            'css'    => true,                   // css-файлы
-            'img'    => true,                   // фото товаров
-            'doc'    => true,                   // файлы документации
-            'cert'   => true,                   // файлы сертификатов
-            'blog'   => true,                   // thumbnails постов блога
-            'banner' => true,                   // баннеры справа
-            'slider' => true,                   // слайдер на главной
+            'js'     => false,                   // js-файлы
+            'css'    => false,                   // css-файлы
+            'img'    => false,                   // фото товаров
+            'doc'    => false,                   // файлы документации
+            'cert'   => false,                   // файлы сертификатов
+            'blog'   => false,                   // thumbnails постов блога
+            'banner' => false,                   // баннеры справа
+            'slider' => false,                   // слайдер на главной
         ),
-        'url'    => 'http://www.tinko.info/',
+        'url'    => 'http://cdn.host2.ru/',
     ),
     'sef' => $routing,                           // см. файл routing.php
     'email' => array(
